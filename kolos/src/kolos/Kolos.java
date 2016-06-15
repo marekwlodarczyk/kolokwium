85582e20eedd7d70c56a7ac62fe7533840583975
marekwlodarczyk <wlodarczyk.marek94@gmail.com>
2016-06-15 13:30:43 +0200

    Dodanie metody zliczajacej

--- kolos/src/kolos/Kolos.java
+++ kolos/src/kolos/Kolos.java
@@ -1,17 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package kolos;
 
 public class Kolos {
+    //Metoda zliczajaca ilosc samochodow
+    public static int count(int[] A) 
+    {
+        int countZero = 0;
+        int countCars = 0;
 
-    /**
-     * @param args the command line arguments
-     */
+        for(int i=0; i<A.length;i++)
+        {
+            if(A[i] == 0)
+            {
+                countZero++;
+            }
+            if(countZero > 0 && A[i]==1 )
+            {
+                countCars += countZero;
+
+            }
+             if(countCars>1000000000)
+             {
+                 return -1;
+             }
+        }
+        return countCars;
+    }
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+       
     }
     
 }
